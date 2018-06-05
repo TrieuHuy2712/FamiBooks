b9e35091ae4daf08e99ae26175862b529ef661a9
danztran <ntctheboy97@gmail.com>
2018-06-05 11:37:14 +0700

    Adding save change button event page

--- app/src/main/java/com/huytrieu/familybook/StatusActivity.java
+++ app/src/main/java/com/huytrieu/familybook/StatusActivity.java
@@ -20,11 +20,35 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.huytrieu.chatstudentapp.R;
 
 public class StatusActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    EditText StatusInput;
+    Button SaveChangesButton;
+    DatabaseReference changeStatusRef;
+    ProgressDialog loadingBar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
+        mAuth = FirebaseAuth.getInstance();
+        String user_id = mAuth.getCurrentUser().getUid();
+        changeStatusRef = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+        toolbar = findViewById(R.id.status_app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Change status");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        loadingBar = new ProgressDialog(this);
+
+        StatusInput = findViewById(R.id.edt_status);
+        SaveChangesButton = findViewById(R.id.btn_save_changes);
+        String old_status = getIntent().getExtras().get("user_status").toString();
+        StatusInput.setText(old_status);
+        SaveChangesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String new_status = StatusInput.getText().toString();
+            }
+        });
     }
 }
