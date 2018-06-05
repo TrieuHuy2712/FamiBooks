780377d9db925798d79ce52c00880ee3e0398f14
danztran <ntctheboy97@gmail.com>
2018-06-05 11:50:50 +0700

    Added  startpage activity

--- app/src/main/java/com/huytrieu/familybook/StartPageActivity.java
+++ app/src/main/java/com/huytrieu/familybook/StartPageActivity.java
@@ -9,10 +9,26 @@ import android.widget.Button;
 import com.huytrieu.chatstudentapp.R;
 
 public class StartPageActivity extends AppCompatActivity {
-
+    Button already_have_account_button,need_account_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+        already_have_account_button = findViewById(R.id.already_have_account_button);
+        need_account_button = findViewById(R.id.need_account_button);
+        already_have_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(StartPageActivity.this,LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+        need_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(StartPageActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
