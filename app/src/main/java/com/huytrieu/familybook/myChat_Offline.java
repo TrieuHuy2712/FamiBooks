f3e701f86dacdf055bc5685cf683998fcd12dc33
lequocluong <lequocluong2401@gmail.com>
2018-06-05 09:07:16 +0700

    add myChat_Offine.java

--- app/src/main/java/com/huytrieu/familybook/myChat_Offline.java
+++ app/src/main/java/com/huytrieu/familybook/myChat_Offline.java
@@ -18,11 +18,42 @@ import com.squareup.picasso.Picasso;
  */
 
 public class myChat_Offline extends Application {
-
+    DatabaseReference UsersReference;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
     @Override
     public void onCreate() {
         super.onCreate();
 
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
+        //load picture offline - picasso
+        Picasso.Builder builder = new Picasso.Builder(this);
+        builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
+        Picasso built = builder.build();
+        built.setIndicatorsEnabled(true);
+        built.setLoggingEnabled(true);
+        Picasso.setSingletonInstance(built);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        if (currentUser!=null){
+            String online_user_id = mAuth.getCurrentUser().getUid();
+            UsersReference = FirebaseDatabase.getInstance().getReference().child("Users").child(online_user_id);
+
+            UsersReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    UsersReference.child("online").onDisconnect().setValue(ServerValue.TIMESTAMP);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
     }
+}
+
