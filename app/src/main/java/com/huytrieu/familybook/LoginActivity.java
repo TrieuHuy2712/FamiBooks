eb7e2a1cd4a2fdf7f82dfade39a12e2cad9f9358
sinbadkhang <singbad1998@gmail.com>
2018-06-08 11:15:57 +0700

    fixed wrong commit

--- app/src/main/java/com/huytrieu/familybook/LoginActivity.java
+++ app/src/main/java/com/huytrieu/familybook/LoginActivity.java
@@ -32,16 +32,81 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.huytrieu.chatstudentapp.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    FirebaseAuth mAuth;
+    private Toolbar mToolbar;
+    EditText edt_login_email, edt_login_password;
+    Button login_button;
+    ProgressDialog loadingBar;
+    DatabaseReference usersreference;
+    LocationManager locationManager;
+    private String provider = "";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mToolbar = findViewById(R.id.login_toolbar);
+        mAuth = FirebaseAuth.getInstance();
+        usersreference = FirebaseDatabase.getInstance().getReference().child("Users");
 
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Sign In");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        loadingBar = new ProgressDialog(this);
+        edt_login_email = findViewById(R.id.edt_login_email);
+        edt_login_password = findViewById(R.id.edt_login_password);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edt_login_email.getText().toString();
+                String password = edt_login_password.getText().toString();
+
+                LoginUserAccount(email, password);
+            }
+        });
     }
 
 
+
+    private void LoginUserAccount(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please write your email", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please write your password", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Log in account");
+            loadingBar.setMessage("Please wait....");
+            loadingBar.show();
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String online_user_id = mAuth.getCurrentUser().getUid();
+                                String DeviceToken = FirebaseInstanceId.getInstance().getToken();
+                                usersreference.child(online_user_id).child("device_token").setValue(DeviceToken)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                startActivity(mainIntent);
+                                                finish();
+                                            }
+                                        });
+
+
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Please check your email or password", Toast.LENGTH_SHORT).show();
+                            }
+                            loadingBar.dismiss();
+                        }
+                    });
+        }
+    }
 }
 
--- app/src/main/java/com/huytrieu/familybook/MainActivity.java
+++ app/src/main/java/com/huytrieu/familybook/MainActivity.java
@@ -27,13 +27,164 @@ import com.google.firebase.database.ServerValue;
 import com.huytrieu.chatstudentapp.R;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
+    private Toolbar mToolbar;
+    private FirebaseAuth mAuth;
+    ViewPager myViewPaper;
+    TabLayout myTabLayout;
+    FirebaseUser currentUser;
+    DatabaseReference UsersReference;
+    TabsPaperAdapter myTabsPaperAdapter;
+    Context context;
+    LocationManager locationManager;
+    private String provider;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myViewPaper = findViewById(R.id.main_tabs_paper);
+        myTabsPaperAdapter = new TabsPaperAdapter(getSupportFragmentManager());
+        myViewPaper.setAdapter(myTabsPaperAdapter);
+        myTabLayout = findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPaper);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            String online_user_id = mAuth.getCurrentUser().getUid();
+            UsersReference = FirebaseDatabase.getInstance().getReference().child("Users").child(online_user_id);
+        }
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("myChat");
+
+        //Get location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (enabled) {
+            Criteria criteria = new Criteria();
+            provider = locationManager.getBestProvider(criteria, false);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Location location = locationManager.getLastKnownLocation(provider);
+            if(location!=null){
+                System.out.println("Provider "+provider +"has been selected.");
+                onLocationChanged(location);
+            }else {
+                String id_au= mAuth.getCurrentUser().getUid();
+                UsersReference.child("latituteField").setValue("Location not available");
+                UsersReference.child("longitudeField").setValue("Location not available");
+            }
+            locationManager.requestLocationUpdates(provider,400,1,this);
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser==null){
+            LogOutUser();
+        }else if (currentUser!=null) {
+            UsersReference.child("online").setValue("true");
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (currentUser!=null) {
+            UsersReference.child("online").setValue(ServerValue.TIMESTAMP);
+        }
+    }
+
+    private void LogOutUser() {
+        Intent startPageIntent = new Intent(MainActivity.this,StartPageActivity.class);
+        startPageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(startPageIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+
+        if (item.getItemId()==R.id.main_logout_button){
+            if (currentUser !=null){
+                UsersReference.child("online").setValue(ServerValue.TIMESTAMP);
+            }
+            mAuth.signOut();
+            LogOutUser();
+        }
+        if (item.getItemId()==R.id.main_account_settings_button){
+            Intent settingsIntent = new Intent(MainActivity.this,SettingsActivity.class);
+            startActivity(settingsIntent);
+        }
+        if (item.getItemId()==R.id.main_all_users_button){
+            Intent allUsersIntent = new Intent(MainActivity.this,AllUsersActivity.class);
+            startActivity(allUsersIntent);
+        }
+        return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double lat= location.getLatitude();
+        double lng= location.getLongitude();
+        String id_au= mAuth.getCurrentUser().getUid();
+        UsersReference.child("latituteField").setValue(String.valueOf(lat));
+        UsersReference.child("longitudeField").setValue(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
 
     }
 
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(this, "Enable new provider "+ s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(this, "Disbled provider "+s, Toast.LENGTH_SHORT).show();
+    }
 }
